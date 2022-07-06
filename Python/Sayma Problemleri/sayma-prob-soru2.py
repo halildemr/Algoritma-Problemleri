42fe8b85b643d77345ac06c1581e2d8f23b93d89
Halil Demir <106595669+halildemr@users.noreply.github.com>
2022-07-06 10:41:57 +0300

    Sayma prob. soru2.py

---
+++ Python/Sayma Problemleri/sayma-prob-soru2.py
@@ -0,0 +1,20 @@
+#================================================================
+# Bir karakter topluluğunda ki sesli harfleri sayan algoritmayı
+# kodlayınız.
+#================================================================
+
+cumle = """Bir karakter topluluğunda ki sesli harfleri sayan algoritmayı
+kodlayınız."""
+
+def sesliHarfsay( c ):
+    
+    karaktersayisi = len( c )
+    sayici = 0
+    
+    for i in range(0,karaktersayisi):
+        if c[i] in ['a','e','ı','i','o','ö','u','ü']:
+            sayici = sayici + 1
+            
+    return sayici
+  
+  
